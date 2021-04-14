4c5c3f6ef2160b624d8693d70d69d4bb094f2050
Ulises502 <43120036+Ulises502@users.noreply.github.com>
2021-04-14 10:16:00 -0300

    Created Map Component
    Installed axios, Leaflet, others modules

--- maps/src/App.vue
+++ maps/src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="app">
-    
+    <mapa/>
   </div>
 </template>
 
 <script>
+import Mapa from "./components/Mapa.vue"
 
 export default {
   name: 'App',
   components: {
-
+    Mapa
   }
 }
 </script>
